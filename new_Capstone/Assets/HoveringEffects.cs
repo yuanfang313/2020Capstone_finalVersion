d991256da09648f04dd123d4f3bb80c7f85057bd
Yuan Fang <42786261+yuanfang313@users.noreply.github.com>
2020-04-01 00:41:36 -0400

    firstCommit

---
+++ new_Capstone/Assets/HoveringEffects.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoveringEffects : MonoBehaviour
+{
+    public AudioSource audioSource_hover;
+    private Vector3 changedScale;
+    private Vector3 originalScale;
+
+    private bool hadHit = false;
+
+    private void Start()
+    {
+        changedScale = new Vector3 (0.7f, 0.7f, 0.7f);
+        originalScale = transform.localScale;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        transform.localScale = changedScale;
+        if (!hadHit)
+        {
+            audioSource_hover.Play();
+            hadHit = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        transform.localScale = originalScale;
+        hadHit = false;
+    }
+}
---
+++ new_Capstone/Assets/triggeringEffects.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class triggeringEffects : MonoBehaviour
+{
+    public AudioSource triggeringSound;
+    public AudioClip triggeringClip_1, triggeringClip_2;
+    private bool triggerHadPlay = false;
+    private GameObject currentHittedObject = null;
+    private bool startTrigger = false;
+
+    private void Awake()
+    {
+        ControllerStatus.TriggerDown += PlayTriggeringSound;
+        PointerStatus.OnPointerUpdateForObject += PlayDependOnHittedObject;
+    }
+
+    private void OnDestroy()
+    {
+        ControllerStatus.TriggerDown -= PlayTriggeringSound;
+        PointerStatus.OnPointerUpdateForObject -= PlayDependOnHittedObject;
+    }
+
+    private void PlayTriggeringSound (bool rightTriggerDown, bool leftTriggerDown)
+    {
+        if(rightTriggerDown || leftTriggerDown)
+        {
+            startTrigger = true;
+            if (!triggerHadPlay)
+            { 
+                PlaySounds();
+                triggerHadPlay = true;
+            }
+        } else
+        {
+            triggerHadPlay = false;
+        }
+    }
+
+    private void PlaySounds()
+    {
+        if (currentHittedObject.tag == "target")
+        {
+            triggeringSound.PlayOneShot(triggeringClip_1);
+        }
+        else if (currentHittedObject != null)
+        {
+            triggeringSound.PlayOneShot(triggeringClip_2);
+        }
+    }
+
+    private void LoadScenes()
+    {
+            if (currentHittedObject.tag == "module1")
+            {
+                SceneManager.LoadScene("Level_1-1");
+            }
+            else if (currentHittedObject.tag == "tutorial")
+            {
+                SceneManager.LoadScene("_Tutorial");
+            }
+            else if (currentHittedObject.tag == "main menu")
+            {
+                SceneManager.LoadScene("MainMenu");
+            }
+    }
+
+    private void PlayDependOnHittedObject(GameObject hitObject)
+    {
+        currentHittedObject = hitObject;
+        if(startTrigger && !triggeringSound.isPlaying)
+        {
+            LoadScenes();
+        }
+    }
+}
